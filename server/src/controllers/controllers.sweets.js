8b33d39d321319fc35c3bf145fa9a7fd96d7a63c
Milan Isac <milan.isac.7@gmail.com>
2025-12-14 10:48:24 +0530

    GREEN: implement sweets pagination

--- server/src/controllers/controllers.sweets.js
+++ server/src/controllers/controllers.sweets.js
@@ -63,3 +63,15 @@ exports.updateSweet = async (req, res) => {
 
   res.json(sweet);
 };
+
+
+exports.getAllSweets = async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const sweets = await Sweet.find().skip(skip).limit(limit);
+  const count = await Sweet.countDocuments();
+
+  res.json({ count, sweets });
+};
