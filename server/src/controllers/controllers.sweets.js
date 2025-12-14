0ac497b9d9dd886ad2fcb86ac301158231621b25
Milan Isac <milan.isac.7@gmail.com>
2025-12-14 10:35:31 +0530

    GREEN: validate sweet required fields

--- server/src/controllers/controllers.sweets.js
+++ server/src/controllers/controllers.sweets.js
@@ -38,3 +38,15 @@ exports.deleteSweet = async (req, res) => {
   res.status(200).json({ message: "Sweet deleted" });
 };
 
+
+exports.createSweet = async (req, res) => {
+  const { name, price, stock } = req.body;
+
+  if (!name || price == null || stock == null) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const sweet = await Sweet.create({ name, price, stock });
+  res.status(201).json(sweet);
+};
+
